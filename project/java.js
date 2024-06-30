d35d4c7f17cc6e6c2e3009d4afbd61eb953da3f2
russordi <russelordinario.01@gmail.com>
2024-06-30 15:42:57 +0800

    Add files via upload

---
+++ project/java.js
@@ -0,0 +1,36 @@
+let menuIcon = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header .navbar a [href*=' + id + ' ]').classList.add('active');
+            })
+        }
+    })
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.getElementById('menu-btn');
+    const menu = document.getElementById('menu');
+  
+    menuToggle.addEventListener('click', function() {
+      menu.classList.toggle('active');
+    });
+  });
+  
